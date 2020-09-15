abea2da610f6d4a5d6ba6e93160dde489eac1ca4
eganal <alex.egan13@icloud.com>
2020-09-15 14:55:36 -0700

    My Django Girls app, first commit

---
+++ pvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/administrator/PycharmProjects/Beeronomics/djangopractice/pvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
